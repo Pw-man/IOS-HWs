5b5d3ff35aabb9dcf49bb828d6d53773b2883806
Pw-man <82800017+Pw-man@users.noreply.github.com>
2021-10-06 23:44:11 +0300

    done unchecked

--- IOSINT-HWs/Navigation/PeseliPhotos.swift
+++ IOSINT-HWs/Navigation/PeseliPhotos.swift
@@ -8,32 +8,28 @@
 
 import UIKit
 
-struct PeselPhoto {
-    var photo: String
-}
-
 struct PeseliPhotos {
     static let photosArray = [
-        PeselPhoto(photo: "pexels-anastasia-prideina-7764991"),
-        PeselPhoto(photo: "pexels-binyamin-mellish-169524"),
-        PeselPhoto(photo: "pexels-charles-roth-2745150"),
-        PeselPhoto(photo: "pexels-dayvison-de-oliveira-silva-6458120"),
-        PeselPhoto(photo: "pexels-dominika-roseclay-895259"),
-        PeselPhoto(photo: "pexels-dominika-roseclay-2023384"),
-        PeselPhoto(photo: "pexels-dominika-roseclay-2679682"),
-        PeselPhoto(photo: "pexels-dominika-roseclay-4084032"),
-        PeselPhoto(photo: "pexels-dominika-roseclay-4193995"),
-        PeselPhoto(photo: "pexels-dominika-roseclay-5565246"),
-        PeselPhoto(photo: "pexels-dominika-roseclay-6792360"),
-        PeselPhoto(photo: "pexels-emily-hopper-3487734"),
-        PeselPhoto(photo: "pexels-igor-morais-3148115"),
-        PeselPhoto(photo: "pexels-ingrid-dietrich-1334553"),
-        PeselPhoto(photo: "pexels-nilanka-sampath-7037995"),
-        PeselPhoto(photo: "pexels-noelle-otto-906065"),
-        PeselPhoto(photo: "pexels-pavel-danilyuk-7055637"),
-        PeselPhoto(photo: "pexels-pixabay-257519"),
-        PeselPhoto(photo: "pexels-rodnae-productions-7516789"),
-        PeselPhoto(photo: "pexels-visually-us-1686790")
+        UIImage(named: "pexels-anastasia-prideina-7764991")!,
+        UIImage(named: "pexels-binyamin-mellish-169524")!,
+        UIImage(named: "pexels-charles-roth-2745150")!,
+        UIImage(named: "pexels-dayvison-de-oliveira-silva-6458120")!,
+        UIImage(named:"pexels-dominika-roseclay-895259")!,
+        UIImage(named:"pexels-dominika-roseclay-2023384")!,
+        UIImage(named:"pexels-dominika-roseclay-2679682")!,
+        UIImage(named:"pexels-dominika-roseclay-4084032")!,
+        UIImage(named:"pexels-dominika-roseclay-4193995")!,
+        UIImage(named:"pexels-dominika-roseclay-5565246")!,
+        UIImage(named:"pexels-dominika-roseclay-6792360")!,
+        UIImage(named:"pexels-emily-hopper-3487734")!,
+        UIImage(named:"pexels-igor-morais-3148115")!,
+        UIImage(named:"pexels-ingrid-dietrich-1334553")!,
+        UIImage(named:"pexels-nilanka-sampath-7037995")!,
+        UIImage(named:"pexels-noelle-otto-906065")!,
+        UIImage(named:"pexels-pavel-danilyuk-7055637")!,
+        UIImage(named:"pexels-pixabay-257519")!,
+        UIImage(named:"pexels-rodnae-productions-7516789")!,
+        UIImage(named:"pexels-visually-us-1686790")!
     ]
 }
 
